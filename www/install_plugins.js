49b09f264b4e300d984d880e7a4d5b43df949d1f
Stephen Delisle <sdelisle@atb.com>
2020-02-10 13:29:36 -0700

    debug

--- www/install_plugins.js
+++ www/install_plugins.js
@@ -1,6 +1,6 @@
 console.log('installing plugins')
 const execSync = require('child_process').execSync;
-execSync("cordova plugin add cordova-plugin-add-swift-support")
-console.log('installing plugins: make sure swift support is good')
+// execSync("cordova plugin add cordova-plugin-add-swift-support")
+// console.log('installing plugins: make sure swift support is good')
 execSync("cordova plugin add berbix-cordova-plugin@1.0.1")
 console.log('installing plugins: berbix-cordova-plugin')
